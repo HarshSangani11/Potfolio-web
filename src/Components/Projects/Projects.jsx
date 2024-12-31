116e29fc5d6921dcd28288ff981ef7b861e47d44
HarshSangani11 <harshsangani2105@gmail.com>
2024-12-31 19:36:34 +0530

    Commited

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,48 +1,10 @@
-// import React from "react";
-// import ProjectCard from "./ProjectCard";
-
-// const Projects = () => {
-//   return (
-//     <div id="Projects" className="p-10 md:p-24 text-white ">
-//       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
-//       <div className="py-12 px-8 flex flex-wrap gap-5">
-//         <ProjectCard
-//           title="Blogging Website"
-//           main="this is a bloggin website created in next js and used some component library used some component library"
-//         />
-//         <ProjectCard
-//           title="Youtue Clone"
-//           main="this is a bloggin website created in next js and used some component library used some component library"
-//         />
-//         <ProjectCard
-//           title="Netflix Clone"
-//           main="this is a blogging website created this ijsdjf l in next js and used some component library used some component library"
-//         />
-//         <ProjectCard
-//           title="Netflix Clone"
-//           main="this is a blogging website created this ijsdjf l in next js and used some component library used some component library"
-//         />
-//         <ProjectCard
-//           title="Netflix Clone"
-//           main="this is a blogging website created this ijsdjf l in next js and used some component library used some component library"
-//         />
-//         <ProjectCard
-//           title="Netflix Clone"
-//           main="this is a blogging website created this ijsdjf l in next js and used some component library used some component library"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// // export default Projects;
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import Con from "../../assets/main.jpg"
 import Vio from "../../assets/hero-1.jpg.webp"
 import Nar from "../../assets/anime-06.jpg"
 import Gam from "../../assets/slider-1.jpg"
-import It from "../../assets/It.jpg"
+import It from "../../assets/it.jpg"
 import Hot from "../../assets/hot.jpg"
 
 const Projects = () => {
@@ -55,7 +17,7 @@ const Projects = () => {
           main="This is a VideoGraph website created in html using a Bootstrap."
           demoUrl="https://videographs.netlify.app/"
           imgUrl={Vio}
-           gitUrl="https://github.com/HarshSangani11/videograph.git"
+          gitUrl="https://github.com/HarshSangani11/videograph.git"
         />
         <ProjectCard
           title="construction"
@@ -68,28 +30,28 @@ const Projects = () => {
           title="Naruto"
           main="This is a VideoGraph website created in html using a Bootstrap."
           imgUrl={Nar}
-           demoUrl="https://narutoteube-web.netlify.app/"
+          demoUrl="https://narutoteube-web.netlify.app/"
           gitUrl="https://github.com/HarshSangani11/Naruto.git"
         />
         <ProjectCard
           title="Game Warrior"
           main="This is a VideoGraph website created in html using a Bootstrap.also use media-query"
           imgUrl={Gam}
-           demoUrl="https://preview.colorlib.com/theme/gamewarrior/"
+          demoUrl="https://preview.colorlib.com/theme/gamewarrior/"
           gitUrl="https://github.com/HarshSangani11/Game.git"
         />
         <ProjectCard
           title="IT-company Website"
           main="This is a IT-company website created in html using a Bootstrap.also use media-query and javascript"
-            imgUrl={It}
-           demoUrl="https://itcompany-web.netlify.app/"
+          imgUrl={It}
+          demoUrl="https://itcompany-web.netlify.app/"
           gitUrl="https://github.com/HarshSangani11/It-web.git"
         />
         <ProjectCard
           title="Social Media App"
           main="A social media app with user authentication, post sharing, and notifications."
           imgUrl={Hot}
-           demoUrl="https://hotelproject-web.netlify.app/"
+          demoUrl="https://hotelproject-web.netlify.app/"
           gitUrl="https://github.com/HarshSangani11/Hotel.git"
         />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//  <ErrorBoundary>
-//       <ChakraProvider>
-//         <App />
-//       </ChakraProvider>
-//     </ErrorBoundary>  </React.StrictMode>,
-// )
-// import React from 'react';
-// import { ChakraProvider } from '@chakra-ui/react';
-// import App from './App';
-// import ErrorBoundary from './ErrorBoundary';
-
-// function Main() {
-//   return (
-//     <ErrorBoundary>
-//       <ChakraProvider>
-//         <App />
-//       </ChakraProvider>
-//     </ErrorBoundary>
-//   );
-// }
-
-// export default Main;
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { ChakraProvider } from '@chakra-ui/react';
@@ -37,9 +7,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-       
-        <App />
-     
- 
-  </React.StrictMode> 
+
+    <App />
+
+
+  </React.StrictMode>
 );
